1902beb2a83d72bc6d1c193c35f2977bd9c33328
Maciej Furman <maciek.furman.user@gmail.com>
2021-03-23 18:52:40 +0100

    fix(MP-1): Remove ChakraProvider from index.tsx (cause it's used in App.tsx already).

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
