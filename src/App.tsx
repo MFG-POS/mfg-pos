a8a2811436ae6d69a4f85326cd36b72ba90c6f59
Dominik Młynarczyk <hello@dominikmlynarczyk.com>
2021-03-27 00:05:08 +0100

    feat(meals-c): Add sample components to test Firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import MealsList from 'components/Meals/MealsList/MealsList';
 import AddSupplier from './AddSupplier';
 
 const App = () => (
   <ChakraProvider>
     <AddSupplier />
+    <MealsList />
   </ChakraProvider>
 );
 
