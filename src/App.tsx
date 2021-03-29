32c087e2db26d36bebcc4ddf0f89352144142990
Maciej Furman <maciek.furman.user@gmail.com>
2021-03-29 20:29:27 +0200

    refactor(MP-34): Remove unused components. Show categories table on temporal main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import MealsList from 'components/Meals/MealsList/MealsList';
 import AddSupplier from './AddSupplier';
+import Categories from './components/tables/Categories';
 
 const App = () => (
   <ChakraProvider>
     <AddSupplier />
+    <Categories />
     <MealsList />
   </ChakraProvider>
 );
