ca396dcd4eb449b33c91cbee0656d9b01c55a1fd
kacpergadula <k.gadula@interia.pl>
2021-04-07 23:03:32 +0200

    MP-15 Integration with React Hook Form

--- src/components/Supplier/AddSupplierButtons.tsx
+++ src/components/Supplier/AddSupplierButtons.tsx
@@ -2,10 +2,10 @@ import { Button, ButtonGroup } from '@chakra-ui/react';
 
 const AddSupplierButtons = () => (
   <ButtonGroup h="4em" w="60em" variant="outline" spacing="6">
-    <Button bg="#33D268" w="6em" h="2em" color="white" colorScheme="#33D268">
+    <Button type="submit" bg="#33D268" w="6em" h="2em" color="white" colorScheme="#33D268">
       Dodaj
     </Button>
-    <Button w="6em" h="2em" bg="white" colorScheme="gray">
+    <Button type="submit" w="6em" h="2em" bg="white" colorScheme="gray">
       Anuluj
     </Button>
   </ButtonGroup>
