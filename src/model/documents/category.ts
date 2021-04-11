7117555f7c08d50cc16de870d6b0c38d54597ee3
Maciej Furman <maciek.furman.user@gmail.com>
2021-04-11 16:11:22 +0200

    fix(MP-29): Fix category model according to arrangements (remove type, add parent property).

--- src/model/documents/category.ts
+++ src/model/documents/category.ts
@@ -1,8 +1,7 @@
 import { BaseModel } from '../base-model';
-import { CategoryType } from '../enums/category-type';
 
 export interface Category extends BaseModel {
   imagePath: string;
   name: string;
-  type: CategoryType;
+  parent?: Category;
 }
