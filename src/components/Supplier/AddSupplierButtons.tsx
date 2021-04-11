9774208c38911cda15fce9f452beddc33dc0f059
kacpergadula <k.gadula@interia.pl>
2021-04-11 21:38:09 +0200

    MP-15 form responsive components

--- src/components/Supplier/AddSupplierButtons.tsx
+++ src/components/Supplier/AddSupplierButtons.tsx
@@ -1,14 +1,29 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, Box } from '@chakra-ui/react';
+import styled from 'styled-components';
 
-const AddSupplierButtons = () => (
-  <ButtonGroup h="4em" w="60em" variant="outline" spacing="6">
-    <Button type="submit" bg="#33D268" w="6em" h="2em" color="white" colorScheme="#33D268">
-      Dodaj
-    </Button>
-    <Button type="submit" w="6em" h="2em" bg="white" colorScheme="gray">
-      Anuluj
-    </Button>
-  </ButtonGroup>
-);
+const AddSupplierButtons = () => {
+  const StyledBox = styled(Box)`
+    min-width: 10%;
+
+    @media screen and (max-width: 900px) {
+      min-width: 10%;
+    }
+  `;
+
+  return (
+    <ButtonGroup h="4em" w="100%" variant="outline" spacing="6" marginTop="1em">
+      <StyledBox>
+        <Button type="submit" bg="#33D268" h="2em" color="white" colorScheme="#33D268">
+          Dodaj
+        </Button>
+      </StyledBox>
+      <StyledBox>
+        <Button type="submit" h="2em" bg="white" colorScheme="gray">
+          Anuluj
+        </Button>
+      </StyledBox>
+    </ButtonGroup>
+  );
+};
 
 export default AddSupplierButtons;
