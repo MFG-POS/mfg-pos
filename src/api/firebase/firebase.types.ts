47975a5869b9120b160c49eb21ea919acdc82bd3
Maciej Furman <maciek.furman.user@gmail.com>
2021-04-17 15:01:41 +0200

    feature(MP-47): Prepare generic method that loads and map documents from firestore. Extend firestore types.

--- src/api/firebase/firebase.types.ts
+++ src/api/firebase/firebase.types.ts
@@ -1,9 +1,8 @@
-/* eslint-disable max-len */
 import firebase from 'api/firebase/firebase.api';
 
-export type Firestore = {
-  Doc: firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
-};
+export type Snapshot = firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>;
+export type Documents = firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
+export type CollectionReference = firebase.firestore.CollectionReference<firebase.firestore.DocumentData>;
 
 // Interface for sample documents inside sample collection called "meals"
 export interface Meal {
