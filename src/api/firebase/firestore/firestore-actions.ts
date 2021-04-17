7466cc2baf4420dd6b1416a84091b440e3b9b5f5
Maciej Furman <maciek.furman.user@gmail.com>
2021-04-17 15:01:41 +0200

    feature(MP-47): Prepare generic method that loads and map documents from firestore. Extend firestore types.

---
+++ src/api/firebase/firestore/firestore-actions.ts
@@ -0,0 +1,13 @@
+import { BaseObject } from 'model/base-object';
+import { CollectionReference, Documents, Snapshot } from '../firebase.types';
+import { firestore } from '../firebase.api';
+
+export const all = async <T extends BaseObject>(collection: string): Promise<T[]> => {
+  const reference: CollectionReference = firestore.collection(collection);
+  const snapshot: Snapshot = await reference.get();
+
+  return (snapshot.docs.map((data: Documents) => ({
+    id: data.id,
+    ...data.data(),
+  })) as unknown) as T[];
+};
