e6e43f343ebb3f858ecd2ee692fbf156e15ce296
Maciej Furman <maciek.furman.user@gmail.com>
2021-04-17 15:12:24 +0200

    feat(MP-24): Prepare separate paginator component.
    Replace simple object type with created BaseObject. Implement searching and pagination mechanisms. Expand table logic according to made changes.

--- src/components/Table/ActionCell.tsx
+++ src/components/Table/ActionCell.tsx
@@ -1,14 +1,15 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { Row } from 'react-table';
-import { TableAction } from '../../model/table/table-definitions';
+import { TableAction } from 'model/table/table-definitions';
+import { BaseObject } from 'model/base-object';
 
-type ActionCellProps<T extends object> = {
+type ActionCellProps<T extends BaseObject> = {
   name: string;
   row: Row<T>;
   actions: TableAction<T>[];
 };
 
-function ActionCell<T extends object>(props: ActionCellProps<T>) {
+function ActionCell<T extends BaseObject>(props: ActionCellProps<T>) {
   return (
     <Menu>
       <MenuButton as={Button}>{props.name}</MenuButton>
--- src/components/Table/Header.tsx
+++ src/components/Table/Header.tsx
@@ -1,12 +1,13 @@
 import { Th, Thead, Tr } from '@chakra-ui/react';
 import { HeaderGroup } from 'react-table';
 import { v4 as uuidv4 } from 'uuid';
+import { BaseObject } from 'model/base-object';
 
-type HeaderProps<T extends object> = {
+type HeaderProps<T extends BaseObject> = {
   headerGroups: Array<HeaderGroup<T>>;
 };
 
-function Header<T extends object>(props: HeaderProps<T>) {
+function Header<T extends BaseObject>(props: HeaderProps<T>) {
   const headerGroups: Array<HeaderGroup<T>> = props.headerGroups.map((group) => {
     const extendedGroup: HeaderGroup<T> = group;
     extendedGroup.id = uuidv4();
@@ -17,7 +18,7 @@ function Header<T extends object>(props: HeaderProps<T>) {
     <Thead>
       {headerGroups.map((headerGroup) => (
         <Tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-          {headerGroup.headers.map((header, ia) => (
+          {headerGroup.headers.map((header) => (
             <Th {...header.getHeaderProps()} key={header.id}>
               {header.render('Header')}
             </Th>
