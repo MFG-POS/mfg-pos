246935dd14990b63d848c16601e35b56361014cb
kacpergadula <k.gadula@interia.pl>
2021-04-25 23:21:23 +0200

    style: :art: chakra responsive styles
    implementation of chakra responsive styles

--- src/components/Supplier/AddSupplier.tsx
+++ src/components/Supplier/AddSupplier.tsx
@@ -1,34 +1,20 @@
 import { VStack, StackDivider, Box, Flex } from '@chakra-ui/react';
 import AddSupplierTitle from 'components/Supplier/AddSupplierTitle';
 import AddSupplierData from 'components/Supplier/AppSupplierData/AddSupplierData';
-import styled from 'styled-components';
 
-const Form = () => {
-  const StyledDiv = styled.div`
-    width: 100%;
-    padding: 0.5rem 0 0 0;
-
-    @media screen and (max-width: 650px) {
-      width: 650px;
-    }
-  `;
-
-  return (
-    <StyledDiv>
-      <Flex w="100%" p="1em">
-        <Box w="22%" h="30em" bg="gray.200" />
-        <Box w="1%" h="1em" bg="white" />
-        <VStack w="77%" divider={<StackDivider borderColor="gray.200" />} spacing={3} align="stretch">
-          <Box h="2em">
-            <AddSupplierTitle />
-          </Box>
-          <Box h="26em" w="100%">
-            <AddSupplierData />
-          </Box>
-        </VStack>
-      </Flex>
-    </StyledDiv>
-  );
-};
+const Form = () => (
+  <Box w="100%" minWidth="30em">
+    <Flex w="100%" p="1em">
+      <VStack w="100%" spacing={3} align="stretch">
+        <Box h="2em">
+          <AddSupplierTitle />
+        </Box>
+        <Box h="26em" w="100%">
+          <AddSupplierData />
+        </Box>
+      </VStack>
+    </Flex>
+  </Box>
+);
 
 export default Form;
--- src/components/Supplier/AddSupplierButtons.tsx
+++ src/components/Supplier/AddSupplierButtons.tsx
@@ -1,29 +1,18 @@
 import { Button, ButtonGroup, Box } from '@chakra-ui/react';
-import styled from 'styled-components';
 
-const AddSupplierButtons = () => {
-  const StyledBox = styled(Box)`
-    min-width: 10%;
-
-    @media screen and (max-width: 900px) {
-      min-width: 10%;
-    }
-  `;
-
-  return (
-    <ButtonGroup h="4em" w="100%" variant="outline" spacing="6" marginTop="1em">
-      <StyledBox>
-        <Button type="submit" bg="#33D268" h="2em" color="white" colorScheme="#33D268">
-          Dodaj
-        </Button>
-      </StyledBox>
-      <StyledBox>
-        <Button type="submit" h="2em" bg="white" colorScheme="gray">
-          Anuluj
-        </Button>
-      </StyledBox>
-    </ButtonGroup>
-  );
-};
+const AddSupplierButtons = () => (
+  <ButtonGroup h="4em" w="100%" variant="outline" spacing="6">
+    <Box minWidth="10%">
+      <Button type="submit" bg="#33D268" h="2em" color="white" colorScheme="#33D268">
+        Dodaj
+      </Button>
+    </Box>
+    <Box minWidth="10%">
+      <Button type="submit" h="2em" bg="white" colorScheme="gray">
+        Anuluj
+      </Button>
+    </Box>
+  </ButtonGroup>
+);
 
 export default AddSupplierButtons;
