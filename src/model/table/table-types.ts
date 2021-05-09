6058faaf10323781108878cd9638da3fb7bab4ff
Dominik Młynarczyk <hello@dominikmlynarczyk.com>
2021-05-09 23:48:12 +0200

    refactor: Change menu naming (use dish & product)

--- src/model/table/table-types.ts
+++ src/model/table/table-types.ts
@@ -1,12 +1,12 @@
-import { Article } from 'model/documents/article';
-import { Recipe } from 'model/documents/recipe';
+import { Product } from 'model/documents/products';
+import { Dish } from 'model/documents/dish';
 import { Category } from 'model/documents/category';
 import { Ingredient } from 'model/documents/ingredient';
 import { Tax } from 'model/documents/tax';
 import { TableDocument } from './table-definitions';
 
-export type TableArticle = Article & TableDocument<Article>;
-export type TableRecipe = Recipe & TableDocument<Recipe>;
+export type TableProduct = Product & TableDocument<Product>;
+export type TableDish = Dish & TableDocument<Dish>;
 export type TableCategory = Category & TableDocument<Category>;
 export type TableIngredient = Ingredient & TableDocument<Ingredient>;
 export type TableTax = Tax & TableDocument<Tax>;
