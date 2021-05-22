9c4ece31834295e97003f0471f8c45da8e8a100c
Maciej Furman <maciek.furman.user@gmail.com>
2021-05-22 20:38:23 +0200

    feat(MP-46): Display table with products.

--- src/views/Menu/Products.tsx
+++ src/views/Menu/Products.tsx
@@ -1,13 +1,8 @@
-import { Heading, Link } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
-import { ROUTE_MENU_FORMS } from 'routing';
+import ProductsTable from 'views/Tables/ProductsTable';
 
 const Products = () => (
   <>
-    <Heading>MOCK: Artykuły</Heading>
-    <Link as={RouterLink} to={ROUTE_MENU_FORMS.PRODUCT.path}>
-      Formularz
-    </Link>
+    <ProductsTable />
   </>
 );
 
