1446e2d1c1e3bd49628829a205c728b0e0659c0b
Maciej Furman <maciek.furman.user@gmail.com>
2021-06-13 20:52:17 +0200

    feat(MP-59) - Create and style components (atoms) that displays order tiles in terminal.

---
+++ src/components/atoms/OrderTilesGroup/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { isEmpty } from 'others/helper-functions';
+import { Divider, Grid } from '@chakra-ui/react';
+import OrderDocumentTile from 'components/atoms/OrderTile';
+import { OrderDocument } from 'model/order/order-types';
+
+type OrderTilesGroupProps = {
+  showDivider: boolean;
+  documents: OrderDocument[];
+  onDocumentClick: (document: OrderDocument) => void;
+};
+
+const OrderTilesGroup = ({ showDivider, documents, onDocumentClick }: OrderTilesGroupProps) => (
+  <>
+    {showDivider && <Divider my="3" orientation="horizontal" />}
+    {!isEmpty(documents) && (
+      <Grid bg="gray.100" gap={6} templateColumns="repeat(4, 1fr)">
+        {documents.map((document) => (
+          <OrderDocumentTile key={document.id} document={document} onClick={onDocumentClick} />
+        ))}
+      </Grid>
+    )}
+  </>
+);
+
+export default OrderTilesGroup;
