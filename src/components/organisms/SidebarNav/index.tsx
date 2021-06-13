90b9bcc6e382ccc12db8407bfa1d5af53936fa79
Maciej Furman <maciek.furman.user@gmail.com>
2021-06-13 20:53:14 +0200

    feat(MP-74) - Add orders and terminal routing.

--- src/components/organisms/SidebarNav/index.tsx
+++ src/components/organisms/SidebarNav/index.tsx
@@ -1,13 +1,14 @@
 import { Accordion } from '@chakra-ui/react';
-import { FaChartBar, FaHamburger, FaDolly, FaLockOpen, FaWrench } from 'react-icons/fa';
+import { FaChartBar, FaDolly, FaHamburger, FaLockOpen, FaShoppingCart, FaWrench } from 'react-icons/fa';
 
-import { ROUTE_ACCESS, ROUTE_MENU, ROUTE_SETTINGS, ROUTE_STATS, ROUTE_WAREHOUSE } from 'routing';
+import { ROUTE_ACCESS, ROUTE_MENU, ROUTE_ORDERS, ROUTE_SETTINGS, ROUTE_STATS, ROUTE_WAREHOUSE } from 'routing';
 
 import NavGroup from 'components/molecules/NavGroup';
 
 const SidebarNav = () => (
   <>
     <Accordion allowToggle w="100%" flexGrow={1} as="nav">
+      <NavGroup title="Zamówienia" links={Object.values(ROUTE_ORDERS)} icon={FaShoppingCart} />
       <NavGroup title="Statystyki" links={Object.values(ROUTE_STATS)} icon={FaChartBar} />
       <NavGroup title="Menu" links={Object.values(ROUTE_MENU)} icon={FaHamburger} />
       <NavGroup title="Magazyn" links={Object.values(ROUTE_WAREHOUSE)} icon={FaDolly} />
