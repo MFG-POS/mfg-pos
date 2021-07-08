52449bdcd7e08bf84b5822b5a131402f3824281c
Dominik Młynarczyk <hello@dominikmlynarczyk.com>
2021-07-08 17:16:25 +0200

    feat(menu): Add ability to save product

--- src/views/Menu/ProductForm.tsx
+++ src/views/Menu/ProductForm.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Button, useToast } from '@chakra-ui/react';
+import { Button, Stack, useToast, Text, VStack, NumberInput, NumberInputField, FormLabel } from '@chakra-ui/react';
 
 import { ROUTE_MENU } from 'routing';
 import { Product } from 'model/documents/products';
@@ -11,49 +11,89 @@ import { requiredErrorMessage } from 'others/form-default-errors';
 import FormTemplate from 'components/templates/FormTemplate';
 import FormGroupInput from 'components/molecules/FormGroupInput';
 import FormGroupSelect from 'components/molecules/FormGroupSelect';
-import FormGroupProductPrice from 'components/molecules/FormGroupProductPrice';
 import FormGroupFile from 'components/molecules/FormGroupFileUpload';
+import { getAll, getSingle, save } from 'api/firebase/firestore/firestore-actions';
+import FormGroupNumber from 'components/molecules/FormGroupNumber';
+import { store } from 'api/firebase/storage/storage-actions';
+import { firestore } from 'api/firebase/firebase.api';
 
 const ProductForm = () => {
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
-  const toast = useToast();
+
+  const [taxesMap, setTaxesMap] = useState<Record<string, string>>({});
+  const [categoriesMap, setCategoriesMap] = useState<Record<string, string>>({});
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors, isSubmitting },
+    control,
   } = useForm<Product>();
 
-  const taxValue = watch('tax');
+  const toast = useToast();
 
-  /*
-   * TODO: Placeholder function, will be replaced with real API request later
-   */
-  const onSubmit: SubmitHandler<Product> = (data) =>
-    new Promise<void>((resolve) => {
-      setTimeout(() => {
-        setIsSubmitted(true);
-        toast({
-          title: 'Artykuł dodany 🙌',
-          status: 'success',
-          duration: 5000,
-          isClosable: true,
-          position: 'bottom-right',
-        });
-        console.log(`Zapisana artykuł: ${data}`);
-        resolve();
-      }, 2000);
+  // TODO: The following code (to the onSubmit function) can be significantly refactored, without the need for a single tax and additional local states.
+  const [grossPrice, setGrossPrice] = useState<number>(0);
+  const [taxValue, setTaxValue] = useState<number>(0);
+
+  const taxDoc = (watch('tax') as unknown) as string;
+  const netValue = watch('netPrice');
+  const overheadValue = watch('overhead');
+
+  const getGrossPrice = (net: number, overhead: number, tax: number) => {
+    const gross = net + (net * tax) / 100 + (net * overhead) / 100;
+    return gross || 0;
+  };
+
+  useEffect(() => {
+    setGrossPrice(getGrossPrice(Number(netValue), Number(overheadValue), Number(taxValue)));
+  }, [taxValue, netValue, overheadValue]);
+  useEffect(() => {
+    if (!taxDoc) {
+      setTaxValue(0);
+      return;
+    }
+    getSingle('/taxes', taxDoc).then((data) => setTaxValue(data?.value as number));
+  }, [taxDoc]);
+
+  const onSubmit: SubmitHandler<Product> = (data) => {
+    const image: File = data.image[0] as File;
+    store('products/', image, async (url) => {
+      await save('products', {
+        ...data,
+        category: firestore.doc(`categories/${data.category}`),
+        tax: firestore.doc(`taxes/${taxDoc}`),
+        grossPrice,
+        image: url,
+      });
+      setIsSubmitted(true);
+      toast({
+        title: 'Artykuł dodany 🙌',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-right',
+      });
     });
+  };
 
-  /*
-   * TODO: To be replaced with DB data
-   */
-  const MOCK_TAX_OPTIONS = ['PTU 8%', 'PTU 22%', 'PTU 23%'];
-  const MOCK_CATEGORY_OPTIONS = ['Śniadania', 'Obiady', 'Pizza', 'Makarony'];
+  useEffect(() => {
+    getAll('taxes').then((data) =>
+      setTaxesMap(
+        data.reduce((acc, tax) => ({ ...acc, [tax.id!]: `${tax.name}, ${tax.value}%` }), {}) as Record<string, string>,
+      ),
+    );
+    getAll('categories').then((data) => {
+      setCategoriesMap(
+        data.reduce((acc, category) => ({ ...acc, [category.id!]: category.name }), {}) as Record<string, string>,
+      );
+    });
+  }, []);
 
   return (
     <FormTemplate onSubmit={handleSubmit(onSubmit)}>
-      {isSubmitted ? <Redirect to={ROUTE_MENU.PRODUCT.path} /> : null}
+      {isSubmitted ? <Redirect to={ROUTE_MENU.PRODUCTS.path} /> : null}
       <FormGroupInput
         label="Artykuł"
         id="name"
@@ -68,30 +108,69 @@ const ProductForm = () => {
         id="category"
         name="category"
         placeholder="Wybierz kategorię"
-        register={register}
         errors={errors}
         validation={{ required: requiredErrorMessage }}
-        options={MOCK_CATEGORY_OPTIONS}
+        options={categoriesMap}
+        control={control}
       />
       <FormGroupSelect
         label="Podatek"
         id="tax"
         name="tax"
         placeholder="Wybierz podatek"
-        register={register}
         errors={errors}
         validation={{ required: requiredErrorMessage }}
-        options={MOCK_TAX_OPTIONS}
+        options={taxesMap}
+        control={control}
       />
-      <FormGroupProductPrice
-        id="product"
-        name="grossPrice"
-        label="Cena artykułu"
-        tax={taxValue}
-        register={register}
-        errors={errors}
-        validation={{ required: requiredErrorMessage }}
-      />
+      {/* TODO: Code in the following div for future refactoring (separating into a separate component, e.g. FormGroupCalculatePrice). */}
+      <div>
+        <Text fontSize="lg" fontWeight="bold" pb="5" textAlign="center" color="gray.700">
+          💰 Cena artykułu
+        </Text>
+        <Stack
+          direction={{ base: 'column', lg: 'row' }}
+          alignItems={{ base: 'center', lg: 'flex-end' }}
+          justifyContent="center"
+        >
+          <VStack>
+            <FormGroupNumber
+              label="Cena netto (zł)"
+              id="netPrice"
+              name="netPrice"
+              register={register}
+              errors={errors}
+              validation={{ required: requiredErrorMessage }}
+            />
+          </VStack>
+          <VStack>
+            <FormGroupNumber
+              label="Narzut / marża (%)"
+              id="overhead"
+              name="overhead"
+              register={register}
+              errors={errors}
+              validation={{ required: requiredErrorMessage }}
+            />
+          </VStack>
+          <VStack>
+            <FormLabel fontSize="sm" fontWeight="md" color="gray.700">
+              Podatek (%)
+            </FormLabel>
+            <NumberInput value={taxValue} isReadOnly>
+              <NumberInputField />
+            </NumberInput>
+          </VStack>
+          <VStack>
+            <FormLabel fontSize="sm" fontWeight="md" color="gray.700">
+              Kwota całkowita (zł)
+            </FormLabel>
+            <NumberInput size="md" value={grossPrice} min={0} precision={2} isReadOnly>
+              <NumberInputField />
+            </NumberInput>
+          </VStack>
+        </Stack>
+      </div>
       <FormGroupFile
         label="Zdjęcie artykułu"
         id="image"
